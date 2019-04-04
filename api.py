5d0dd56b5353c059183f90c246c6fee7d66e9cae
David James <djames7776@gmail.com>
2019-04-04 20:17:46 +0100

    split User class to create multiple routes

--- api.py
+++ api.py
@@ -35,8 +35,16 @@ class User(Resource):
                 return user, 200
         return "User not Found", 404
     
+    
+class DeleteUser(Resource):    
+    def delete(self,name):
+        global users
+        users = [user for user in users if user["name"] !=name]
+        return "{}  is deleted.".format(name), 200
+
+class NewUser(Resource):
     # create new user. Use POST method
-    def post(self, command):
+    def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument("name")
         parser.add_argument("age")
@@ -54,32 +62,26 @@ class User(Resource):
         }
         users.append(user)
         return user, 201
-    
-    def put(self, name):
+
+class UpdateUser(Resource):
+    def put(self,name):
         parser = reqparse.RequestParser()
+        parser.add_argument("name")
         parser.add_argument("age")
         parser.add_argument("job")
         args = parser.parse_args()
 
         for user in users:
             if(name == user["name"]):
+                user["name"]= args["name"]
                 user["age"] = args["age"]
                 user["job"] = args["job"]
                 return user, 200
-        
-        user = {
-            "name": name,
-            "age": args["age"],
-            "job": args["job"]
-        }
-        users.append(user)
-        return user, 201
-    
-    def delete(self,name):
-        global users
-        users = [user for user in users if user["name"] !=name]
-        return "{}  is deleted.".format(name), 200
-    
-api.add_resource(User, "/user/<string:command>")
+        return "user doesn't exist, to create a new user use /newuser route", 404
+api.add_resource(User, "/user/<string:name>")
 api.add_resource(AllUsers,"/users" )
+api.add_resource(NewUser,"/newuser")
+api.add_resource(UpdateUser,"/user/<string:name>/update")
+api.add_resource(DeleteUser,"/user/<string:name>/delete")
+
 app.run(debug=True)
